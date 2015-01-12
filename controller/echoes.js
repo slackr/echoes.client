efce560f84e11f98702beb318b370e258eb62e3a
adrian <adrian@slacknet>
2015-01-11 19:26:36 -0500

    ui improvements

--- controller/echoes.js
+++ controller/echoes.js
@@ -1,117 +1,53 @@
 var $me = null;
 var $ec = null;
 var $ui = null;
+var $session_id = null;
 
 var $keychain = {};
 var socket = null;
 
 $(document).ready(function() {
-    var session_id = null;
     $ec = new EchoesCrypto();
     $ui = new EchoesUi();
 
-    get_me();
+    $ui.get_me();
 
-    var socket_query = encodeURI('session_id=' + session_id + '&nickname=' + $me);
-    $ui.log("connecting to " + AppConfig.WS_SERVER + "?" + socket_query + " as '" + $me + "' with session_id: " + session_id, 1);
+    $(window).keydown(function(event) {
+        if (! (event.ctrlKey
+               || event.metaKey
+               || event.altKey)) {
+            if ($ui.ui.me.is(':visible')) {
+                $ui.ui.me_input.focus();
+            } else {
+                $ui.ui.input.focus();
+            }
+        }
 
-    socket = io(AppConfig.WS_SERVER, {
-        query: socket_query,
-    });
+        if (event.which == 13) {
+            if ($ui.ui.me.is(':visible')) {
+                $me = $ui.ui.me_input.val();
+                if (! $me) {
+                    return;
+                }
 
-    socket.on('error', function(e) {
-        $ui.error(e);
-    });
-    socket.on('reconnect', function() {
-        join_channels();
-    });
-    socket.on('connect', function() {
-        $ui.status('Connected! Say something...');
-    });
-    socket.on('disconnect', function() {
-        $ui.status('Disconnected :(');
-    });
+                init_socket();
+                $ui.hide_me();
+                return;
+            }
 
-    $ui.ui.form.submit(function(){
-        try{
+            if (! socket.connected) {
+                $ui.status('Not connected!');
+                return;
+            }
             send_echo();
-        } catch (e) {
-            $ui.log('fatal: ' + e.toString());
-        }
-        return false;
-    });
-
-
-    socket.on('*me', function(me) {
-        $me = me;
-        $ui.status('You are ' + $me);
-    });
-
-    socket.on('*join', function(join) {
-        if (join.nickname == $me) {
-            $ui.remove_channel(join.channel);
-            $ui.add_channel(join.channel);
-        }
-
-        $ui.status(join.nickname + ' joined ' + join.channel);
-    });
-    socket.on('*part', function(part) {
-        if (part.nickname == $me) {
-            $ui.remove_channel(part.channel);
-        }
-        $ui.status(part.nickname + ' parted ' + part.channel);
-    });
-
-    socket.on('*ulist', function(list){
-        $ui.clear_channels();
-
-        for (var i in list.channels) {
-            $ui.add_channel(list.channels[i]);
         }
     });
-    socket.on('*list', function(list){
-        $ui.status('Channel listing: ' + list.channels.join(', '));
-    });
-    socket.on('*who', function(who){
-        $ui.status('Active nicknames: ' + who.nicknames.join(', '));
-    });
-
-    socket.on('*echo', function(echo){
-        $ui.status(echo.nickname + ' ))) ' + (echo.channel ? echo.channel : '*') + ': ' + echo.echo);
-    });
-
-    socket.on('*keyx', function(data){
-        $ui.log('keyx incoming: ' + data.from + '@me' + ': ' + JSON.stringify(data), 0);
-        keyx_import(data);
-    });
-    socket.on('*keyx_sent', function(data){
-        $ui.status('Encryption key sent to ' + data.to);
-        $ui.log('keyx sent to: ' + data.to + ': ' + JSON.stringify(data), 0);
-    });
-    socket.on('*eecho', function(data){
-        $ui.log('eecho incoming: ' + data.from + ': ' + JSON.stringify(data), 0);
-        decrypt_eecho(data.from, data.echo);
-    });
-    socket.on('*eecho_sent', function(data){
-        $ui.log('eecho sent: ' + data.to + ': ' + JSON.stringify(data));
-    });
-
-    socket.on('*error', function(message){
-        $ui.error(message);
-        $ui.log(message, 3);
-    });
-
-    $ui.ui.input.focus();
 });
 
 function send_echo() {
-    if (! socket.connected) {
-        $ui.status('Not connected!');
-        return;
-    }
     var echo = $ui.ui.input.val();
     var split = echo.trim().split(' ');
-    var channel = $ui.active_channel().val();
+    var channel = $ui.active_channel().text();
 
     if (echo == '') {
         $ui.ui.input.focus();
@@ -167,8 +103,8 @@ function execute_command(params) {
 function join_channels() {
     var joined_channels = [];
     $ui.log('Auto-joining channels...', 1);
-    $ui.joined_channels.each(function() {
-        execute_command(['/join', $(this).val()]);
+    $ui.joined_channels().each(function() {
+        execute_command(['/join', $(this).text()]);
     });
 }
 
@@ -215,8 +151,110 @@ function decrypt_eecho(nick, echo) {
     });
 }
 
-function get_me() {
-    while (! $me) {
-        $me = prompt('What do they call you?');
-    }
+function init_socket() {
+    var socket_query = encodeURI('session_id=' + $session_id + '&nickname=' + $me);
+
+    socket = null;
+    socket = io(AppConfig.WS_SERVER, {
+        query: socket_query,
+        forceNew: true,
+    });
+
+    setup_callbacks();
+
+    $ui.log("connecting to " + AppConfig.WS_SERVER + "?" + socket_query + " as '" + $me + "' with session_id: " + $session_id, 1);
+}
+
+function setup_callbacks() {
+    socket.on('*me', function(me) {
+        $me = me;
+        $ui.status('You are ' + $me + ', say something...');
+    });
+
+    socket.on('*join', function(join) {
+        if (join.nickname == $me) {
+            $ui.remove_channel(join.channel);
+            $ui.add_channel(join.channel);
+        }
+
+        $ui.status((join.nickname == $me ? 'You have' : join.nickname) + ' joined ' + join.channel);
+    });
+    socket.on('*part', function(part) {
+        if (part.nickname == $me) {
+            $ui.remove_channel(part.channel);
+        }
+        $ui.status((part.nickname == $me ? 'You have' : join.nickname) + ' parted ' + part.channel);
+    });
+
+    socket.on('*ulist', function(list){
+        $ui.clear_channels();
+
+        for (var i in list.channels) {
+            $ui.add_channel(list.channels[i]);
+        }
+    });
+    socket.on('*list', function(list){
+        $ui.status('Channel listing: ' + list.channels.join(', '));
+    });
+    socket.on('*who', function(who){
+        $ui.status('Active nicknames: ' + who.nicknames.join(', '));
+    });
+
+    socket.on('*echo', function(echo){
+        $ui.echo(echo.nickname + ' ))) ' + (echo.channel ? echo.channel : '') + ' ' + echo.echo);
+    });
+
+    socket.on('*keyx', function(data){
+        $ui.log('keyx incoming: ' + data.from + '@me' + ': ' + JSON.stringify(data), 0);
+        keyx_import(data);
+    });
+    socket.on('*keyx_sent', function(data){
+        $ui.status('Encryption key sent to ' + data.to);
+        $ui.log('keyx sent to: ' + data.to + ': ' + JSON.stringify(data), 0);
+    });
+    socket.on('*eecho', function(data){
+        $ui.log('eecho incoming: ' + data.from + ': ' + JSON.stringify(data), 0);
+        decrypt_eecho(data.from, data.echo);
+    });
+    socket.on('*eecho_sent', function(data){
+        $ui.log('eecho sent: ' + data.to + ': ' + JSON.stringify(data));
+    });
+
+    socket.on('*error', function(message){
+        $ui.error(message);
+        $ui.log(message, 3);
+    });
+
+    socket.on('error', function(e) {
+        switch (e) {
+            case 'invalid_nick':
+                $me = null;
+                $ui.get_me('Bad nickname :( try again');
+            break;
+            case 'nick_exists':
+                $me = null;
+                $ui.get_me('Nickname exists :( try again');
+            break
+            default:
+                $ui.error(e);
+            break
+        }
+    });
+
+    socket.on('connect_error', function() {
+        console.log('connect error');
+    });
+    socket.on('reconnect_error', function() {
+        console.log('reconnect error');
+    });
+
+    socket.on('reconnect', function() {
+        join_channels();
+    });
+    socket.on('connect', function() {
+        $ui.status('Connected!');
+    });
+    socket.on('disconnect', function() {
+        $ui.status('Disconnected :(');
+    });
 }
--- view/ui.js
+++ view/ui.js
@@ -2,19 +2,57 @@ function EchoesUi() {
     EchoesObject.call(this, 'ui');
 
     this.ui = {
-        window: $("#window"),
+        wall: $("#wall"),
         echoes: $("#echoes"),
-        input: $('#e'),
-        channels: $('#c'),
+        input: $('#echo_input'),
+        me_input: $('#me_input'),
+        me: $('#me'),
+        channels: $('#channels'),
         form: $('form'),
+        buttons: {
+            nicknames: $('#menu_nicknames'),
+            channels: $('#menu_channels'),
+        },
+        lists: {
+            close_lists: $('#close_lists'),
+            nicknames: $('#nicknames'),
+            channels: $('#channels'),
+        }
     }
+
+    this.attach_events();
 }
 
 EchoesUi.prototype = Object.create(EchoesObject.prototype); // inherit EchoesObject
 EchoesUi.prototype.constructor = EchoesUi;
 
+EchoesUi.prototype.attach_events = function() {
+    var self = this;
+
+    this.ui.lists.close_lists.click(function() {
+        self.ui.lists.close_lists.hide();
+        self.ui.lists.nicknames.hide("slide", { direction: "right" }, 100);
+        self.ui.lists.channels.hide("slide", { direction: "left" }, 100);
+
+        self.ui.input.focus();
+    });
+
+    this.ui.buttons.nicknames.click(function() {
+        self.ui.lists.close_lists.show();
+        self.ui.lists.nicknames.toggle("slide", { direction: "right" }, 100);
+    });
+    this.ui.buttons.channels.click(function() {
+        self.ui.lists.close_lists.show();
+        self.ui.lists.channels.toggle("slide", { direction: "left" }, 100);
+    });
+
+    this.ui.lists.channels.on('click', 'li', function() {
+        self.select_channel(this);
+    });
+}
+
 EchoesUi.prototype.scroll_down = function() {
-    this.ui.window.scrollTop(this.ui.window.prop("scrollHeight"));
+    this.ui.wall.scrollTop(this.ui.wall.prop("scrollHeight"));
 };
 
 EchoesUi.prototype.echo = function(echo) {
@@ -36,26 +74,48 @@ EchoesUi.prototype.error = function(error) {
 };
 
 EchoesUi.prototype.active_channel = function() {
-    return this.ui.channels.find('option:selected');
+    return this.ui.channels.find('li[selected="selected"]');
 }
 
 EchoesUi.prototype.joined_channels = function() {
-    return this.ui.channels.find('option');
+    return this.ui.channels.find('li');
 }
 
 EchoesUi.prototype.remove_channel = function(chan) {
-    $ui.channels.find("option[value='" + chan + "']").remove();
+    this.ui.lists.channels.find('li:contains("' + chan + '")').remove();
 }
 
 EchoesUi.prototype.clear_channels = function() {
-    $ui.channels.html('');
+    this.ui.lists.channels.html('');
 }
 
 EchoesUi.prototype.add_channel = function(chan) {
-    $ui.channels.append(
-        $('<option>')
-            .val(chan)
-            .html(chan)
-            .attr('selected', 'selected')
-    );
+    var chan_element =
+        $('<li>')
+            .text(chan)
+
+    this.ui.lists.channels.append(chan_element);
+    this.select_channel(chan_element);
+}
+
+EchoesUi.prototype.select_channel = function(chan_element) {
+    this.joined_channels().removeClass('selected_channel');
+    $(chan_element).addClass('selected_channel').attr('selected','selected');
+}
+
+EchoesUi.prototype.get_me = function(message) {
+    var self = this;
+    message = message || 'What do they call you?';
+
+    this.ui.me_input.attr('placeholder', '');
+    this.ui.me_input.val('');
+    this.ui.me.fadeIn('fast', function() {
+        self.ui.me_input.attr('placeholder', message);
+    });
+    this.ui.me_input.focus();
+}
+
+EchoesUi.prototype.hide_me = function() {
+    this.ui.me_input.val(':)');
+    this.ui.me.fadeOut('slow');
 }
