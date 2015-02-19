c0fa7027971f468405da46678e5dd89a356e625d
adrian <ap@slacknet>
2015-02-19 14:07:16 -0500

    removed object tests

--- test/spec/spec_object.js
+++ test/spec/spec_object.js
@@ -5,23 +5,8 @@ describe("EchoesObject", function() {
         o = new EchoesObject();
     });
 
-    it("should log to log_levels only twice", function() {
-        AppConfig.CONSOLE_LOG = false;
+    it("stub, no tests yet", function() {
         AppConfig.LOG_LEVEL = 1;
-
-        o.log('Test entry debug', 0);
-        o.log('Test entry info', 1);
-        o.log('Test entry error', 3);
-        expect(o.log_entries.length).toEqual(2);
-    });
-
-    it("should log to console only", function() {
-        AppConfig.CONSOLE_LOG = true;
-        AppConfig.LOG_LEVEL = 0;
-
-        o.log('Test entry debug', 0);
-        o.log('Test entry info', 1);
-        o.log('Test entry error', 3);
-        expect(o.log_entries.length).toEqual(0);
+        expect(true).toBe(true);
     });
 });
